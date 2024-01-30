685df90c8ce91e8aff5b8cdc43ff691558e8f25b
aartidevlaravel <>
2024-01-30 07:25:46 +0530

    next to react

--- src/components/Tooltip.jsx
+++
@@ -1,9 +0,0 @@
-const Tooltip = ({ title, children }) => {
-    return (
-        <div className="group flex relative  w-fit">
-            <p className="">{children}</p>
-            <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-8 opacity-0 m-4 mx-auto">{title}</span>
-        </div>
-    )
-}
-export default Tooltip
--- src/redux/ReduxProvider.jsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { Provider } from "react-redux"
-import { Store } from "./Store"
-
-const ReduxProvider = ({ children }) => {
-  return (<Provider store={Store} >{children}</Provider>)
-}
-
-export default ReduxProvider
